3cefd41650eacd87ba81db2c674dee2a368eb77f
anibal317 <anibal317@hotmail.com>
2020-03-31 20:19:26 -0300

    Prueba en el js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
-let strLinkStyleSheet=document.getElementById('lnkStyleSheet').href;
-console.log(strLinkStyleSheet);
+let cssInUse=document.getElementById('lnkStyleSheet');
+let btnChooseThem=document.getElementById('btn_chooseThem');
+
+btnChooseThem.addEventListener('click', chooseThem);
+
+function chooseThem(){
+    console.log(cssInUse.classList.toggle.name.toString);
+}
