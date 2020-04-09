89620623907b00ad2f8481a7ab299e5dc4c7e4da
anibal317 <anibal317@hotmail.com>
2020-04-09 19:54:29 -0300

    Se crea la función para split de tags

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ let dropdownContent=document.getElementById('dropdown-content');
 let btnSearch=document.getElementById('btn_search');
 let txtSearch=document.getElementById('txtSearch');
 let img_suggest=document.getElementById('img_suggest');
+let img_trending=document.getElementById('frmImgsTrending');
 let btn_misGifos=document.getElementById('btn_misGifos');
 let maxSuggestedResult=4;
 
@@ -22,6 +23,7 @@ dayBox.addEventListener('click', toDayTheme);
 nightBox.addEventListener('click', toNightTheme);
 /*btn_misGifos.addEventListener('click',trending);*/
 
+seggest();
 trending();
 
 /*Sección de funciones*/
@@ -45,8 +47,8 @@ function searhGifOs(){
     console.log(txtSearch.value);
 }
 
-async function trending() {
-    let resultado = await fetch("https://api.giphy.com/v1/gifs/trending?api_key=M2w3WvZMLnWs5ra5f7CsLTKJEwaGWD1O&limit="+maxSuggestedResult+"&rating=G")
+async function seggest() {
+    let resultado = await fetch("https://api.giphy.com/v1/gifs/trending?api_key=M2w3WvZMLnWs5ra5f7CsLTKJEwaGWD1O&limit="+maxSuggestedResult+"&rating=G&lang=en")
       .then(respuesta => respuesta.json())
       .then((dato) => dato);
       
@@ -67,4 +69,30 @@ async function trending() {
   }
 
   
-  
+  async function trending() {
+    let resultado = await fetch("https://api.giphy.com/v1/gifs/trending?api_key=M2w3WvZMLnWs5ra5f7CsLTKJEwaGWD1O&limit=2&rating=PG&lang=en")
+      .then(respuesta => respuesta.json())
+      .then((dato) => dato);
+      
+    img_suggest.innerHTML = ' ';
+      
+    resultado.data.forEach((element, index) => {
+        img_trending.innerHTML +=` 
+                        <div class="frmImgsTrending">
+                            <img class="img_trending1" id="-${element['id']}" alt="loading img" src="${element['images'].downsized_large.url}">
+                            <div class="frmTrendingTitle">
+                                <p class="trendingTitle">${splitSlug(element['slug']).trim()}</p>
+                            </div>
+                        </div>
+                        `;
+        //<img class="img_daily_" id="${element['id']}" alt="load${element['title']}ing img" src="${element['images'].original.url}">
+    });
+  }
+
+  function splitSlug(strSlug){
+      strfinal='';
+    strSlug.split("-").forEach(element => strfinal+="#"+element+" ");
+    return strfinal;
+}
+
+  splitSlug("kristen-stewart-ok-kstew-HynSPcwIlaFVe");
--- test/test.js
+++ test/test.js
@@ -1,19 +1,5 @@
-let imgDOM=document.getElementById('img');
-let btn=document.getElementById('btn');
+function splitSlug(strSlug){
+  strSlug.split("-").forEach(element => console.log("#"+element));
+}
 
-btn.addEventListener('click', trending);
-
-
-
-async function trending() {
-    let resultado = await fetch("https://api.giphy.com/v1/gifs/trending?api_key=M2w3WvZMLnWs5ra5f7CsLTKJEwaGWD1O&limit=2&rating=G")
-      .then(respuesta => respuesta.json())
-      .then((dato) => dato);
-      
-      imgDOM.innerHTML = ' ';
-      
-    resultado.data.forEach((element, index) => {
-        imgDOM.innerHTML +=`<div class="text" id="${element['id']}" alt="${element['title']}"><img src="${element['images'].downsized_large.url}"></div>`;
-    });
-  }
-
+splitSlug("kristen-stewart-ok-kstew-HynSPcwIlaFVe");
