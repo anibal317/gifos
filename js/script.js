5421276e209f04d19466bafd71ae2b47cd59d1bc
anibal317 <anibal317@hotmail.com>
2020-04-17 11:59:33 -0300

    Se agrega la parte de búsqueda, falta dropdown

--- js/script.js
+++ js/script.js
@@ -6,47 +6,50 @@ let dayBox=document.getElementById('dayBox');
 let nightBox=document.getElementById('nightBox');
 let btnChooseThem=document.getElementById('btn_downArrow');
 let dropdownContent=document.getElementById('dropdown-content');
-let btnSearch=document.getElementById('btn_search');
 let txtSearch=document.getElementById('txtSearch');
 let img_suggest=document.getElementById('img_suggest');
 let img_trending=document.getElementById('imgTrending');
-let btn_misGifos=document.getElementById('btn_misGifos');
-let maxSuggestedResult=4;
-let btn=document.getElementById('frmImgsTrending');
-let title=document.getElementById('frmTrendingTitle');
+let btnSearch=document.getElementById('btn_search');
+let img_mySearchs=document.getElementById('img_mySearchs');
+let tituloMySearchs=document.getElementById('listSearchs');
+const maxSuggestedResult=4;
+let strBusqueda='';
 
+/*Obtengo en variables las Secciones Generales*/
+let sectionSearchs=document.getElementsByClassName('searchs');
+let sectionSuggestions=document.getElementsByClassName('suggestions');
+let sectionTrending=document.getElementsByClassName('trending');
+let sectionMySearchs=document.getElementsByClassName('mySearchs');
 
 /*Creo eventos listener*/
 btnThemDay.addEventListener('click', toDayTheme);
 btnThemNight.addEventListener('click', toNightTheme);
 btnChooseThem.addEventListener('click', chooseThem);
 dropdownContent.addEventListener('mouseleave', hideChooseThem);
-btn_search.addEventListener('click', searhGifOs);
+btnSearch.addEventListener('click', searchGifs);
+txtSearch.addEventListener('keyup', searhGifOs);
 dayBox.addEventListener('click', toDayTheme);
 nightBox.addEventListener('click', toNightTheme);
 
 
-seggest();
+suggest();
 trending();
 
-window.addEventListener('scroll', function(){
-    if((parseInt(document.scrollingElement.scrollHeight)-parseInt(window.scrollY)==341)){
-        console.log(parseInt(document.scrollingElement.scrollHeight)-parseInt(window.scrollY));
-        //trending();
-    }else{
-        console.log("segui scrolleando");
-        console.log(parseInt(document.scrollingElement.scrollHeight)-parseInt(window.scrollY));
-    }
-})
+
+/**Oculto la sección de búsqueda */
+//sectionSearchs[0].hidden = true;
+//sectionSuggestions[0].hidden = true;
+//sectionTrending[0].hidden=true;
+sectionMySearchs[0].hidden=true;
+
 /*Sección de funciones*/
+
 function toDayTheme(){
     cssInUse.href='css/dayStyle.css';
 }
-
 function toNightTheme(){
     cssInUse.href='css/nightStyle.css';
 }
-
 function chooseThem(){
     if (dropdownContent.style.display=='block'){
         dropdownContent.style.display = "none";
@@ -54,16 +57,13 @@ function chooseThem(){
         dropdownContent.style.display = "block";
     }
 }
-
 function hideChooseThem(){
     dropdownContent.style.display="none";
 }
-
-function searhGifOs(){
-    console.log(txtSearch.value);
+function searhGifOs(){ //dropdown
+    strBusqueda = txtSearch.value;
 }
-
-async function seggest() {
+async function suggest() {
     let resultado = await fetch("https://api.giphy.com/v1/gifs/trending?api_key=NzreMCTPOd9ZcGAzVKXEkG2zPoJnL0bW&limit="+maxSuggestedResult+"&rating=G&lang=es")
       .then(respuesta => respuesta.json())
       .then((dato) => dato);
@@ -81,14 +81,13 @@ async function seggest() {
                         </div>
                         `;
     });
-  }
-  
-  async function trending() {
-    let resultado = await fetch("https://api.giphy.com/v1/gifs/trending?api_key=M2w3WvZMLnWs5ra5f7CsLTKJEwaGWD1O&limit=4&rating=PG-13&lang=en")
-      .then(respuesta => respuesta.json())
-      .then((dato) => dato);
+} 
+async function trending() {
+    let resultado = await fetch("https://api.giphy.com/v1/gifs/trending?api_key=M2w3WvZMLnWs5ra5f7CsLTKJEwaGWD1O&limit=24&rating=PG-13&offset=5&lang=en")
+        .then(respuesta => respuesta.json())
+        .then((dato) => dato);
       
-      img_trending.innerHTML = ' ';
+    img_trending.innerHTML = ' ';
       
     resultado.data.forEach((element, index) => {
         img_trending.innerHTML +=` 
@@ -100,20 +99,40 @@ async function seggest() {
             </div>
                  `;
     });
+}
+async function searchGifs() {
+    sectionSearchs[0].hidden = false;
+    sectionSuggestions[0].hidden = true;
+    sectionTrending[0].hidden=true;
+    sectionMySearchs[0].hidden=false;
     
-  }
+    txtSearch.value="";
 
+    let resultado = await fetch("https://api.giphy.com/v1/gifs/search?api_key=M2w3WvZMLnWs5ra5f7CsLTKJEwaGWD1O&q="+strBusqueda+"&limit=12&offset=0&rating=G&lang=en")
+    .then(respuesta => respuesta.json())
+    .then((dato) => dato);
+    
+    img_mySearchs.innerHTML = ' ';
+    tituloMySearchs.innerText += " "+strBusqueda;
+    resultado.data.forEach((element, index) => {
+    img_mySearchs.innerHTML +=` 
+                      <div class="imgFrameSearch">
+                          <div class="imgFrameSearchTitle">
+                              <p class="titleSearchs">${element['title']}</p>
+                          </div>
+                          <img class="imgSearchs1" id="${element['id']}" alt="loading img" src="${element['images'].downsized_large.url}">
+                      </div>
+                      `;   
+});
+}
 function yesTitle(){
     event.toElement.nextElementSibling.style.display="block";
 }
-
 function noTitle(){
     event.target.lastElementChild.style.display="none";
 }
-
 function splitSlug(strSlug){
     strfinal='';
     strSlug.split("-").forEach(element => strfinal+="#"+element+" ");
     return strfinal;
 }
-
