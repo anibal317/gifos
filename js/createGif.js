47855b2a576dd684d37aa56e68a5a3a7b849e7ab
anibal317 <anibal317@hotmail.com>
2020-04-19 21:51:50 -0300

    Se termina pagina 3 y se empezó la 4

--- js/createGif.js
+++ js/createGif.js
@@ -3,6 +3,8 @@ let btnDropdown=document.getElementsByClassName('dropdown');
 let btnMisGifos=document.getElementsByClassName('btn_misGifos');
 let btnCancelar=document.getElementById('btnCancelar');
 let btnComenzar=document.getElementById('btnComenzar');
+let btnCapturar=document.getElementById('btnCapturar');
+let btnListo=document.getElementById('btnListo');
 
 let sectionMisGifos=document.getElementsByClassName('myGuifos');
 let pag1=document.getElementsByClassName('frmPag1');
@@ -11,10 +13,11 @@ let pag3=document.getElementsByClassName('frmPag3');
 let pag4=document.getElementsByClassName('frmPag4');
 let pag5=document.getElementsByClassName('frmPag5');
 let pag6=document.getElementsByClassName('frmPag6');
-let pag7=document.getElementsByClassName('frmPag7');
 
 btnCancelar.addEventListener('click', ()=>console.log("Cancelar"));
 btnComenzar.addEventListener('click', pagina2);
+btnCapturar.addEventListener('click', pagina3);
+btnListo.addEventListener('click', pagina4);
 
 
 btnCreateGifo[0].hidden=true;
@@ -23,15 +26,23 @@ btnMisGifos[0].hidden=true;
 sectionMisGifos[0].hidden=false;
 
 pag1[0].hidden=true;
-pag2[0].hidden=false;
+pag2[0].hidden=true;
 pag3[0].hidden=true;
-pag4[0].hidden=true;
+pag4[0].hidden=false;
 pag5[0].hidden=true;
 pag6[0].hidden=true;
-pag7[0].hidden=true;
 
 function pagina2(){
     pag1[0].hidden=true;
     pag2[0].hidden=false;
     sectionMisGifos[0].hidden=true;
+}
+function pagina3(){
+    pag2[0].hidden=true;
+    pag3[0].hidden=false;
+}
+function pagina4(){
+    pag3[0].hidden=true;
+    pag4[0].hidden=false;
+
 }
