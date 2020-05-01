c58ebf0c5e2a0167251b78d256f68c79724cff11
anibal317 <anibal317@hotmail.com>
2020-05-01 17:39:02 -0300

    Se agrega flecha para volver atrás, se agrega el copy link, acción post, falta integración

--- js/createGif.js
+++ js/createGif.js
@@ -1,14 +1,14 @@
-let dd = 0;
-let ho = 0;
-let mn = 0;
-let sg = 0;
-let cs = 0;
+let dd = 0; let ho = 0; let mn = 0; let sg = 0; let cs = 0;
      
 let video = document.getElementById("vCamera");
 let video1 = document.getElementById("vCamera1");
 let form = new FormData();
 let gifDone;
 let objectUrl;
+let gifoUrl="https://giphy.com/embed/";
+let strTags='';
+
+arrMyGifos=JSON.parse(localStorage.getItem('myGifos')) || [];
 
 
 let btnCreateGifo=document.getElementsByClassName('btn_createGifo');
@@ -22,6 +22,13 @@ let btnSubirGuifo=document.getElementById('btnSubirGuifo');
 let preViewGifo=document.getElementById('preView');
 let btnRepetir=document.getElementById('btnRepetir');
 let btnCancelarSubida=document.getElementById('btnCancelarSubida');
+let bntCopyLink=document.getElementById('bntCopyLink');
+let btnDownloadGif=document.getElementById('btnDownloadGif');
+let btnReady=document.getElementById('btnReady');
+let tags=document.getElementById('tags');
+let txtTags=document.getElementById('txtTags');
+let txtfinalUrl=document.getElementById('txtfinalUrl');
+let message=document.getElementById('message');
 
 let sectionMisGifos=document.getElementsByClassName('myGuifos');
 let pag1=document.getElementsByClassName('frmPag1');
@@ -37,20 +44,23 @@ btnCapturar.addEventListener('click', pagina3);
 btnRepetir.addEventListener('click', repeat);
 btnListo.addEventListener('click', pagina4);
 btnSubirGuifo.addEventListener('click', pagina5);
-// btnCancelarSubida.addEventListener('click', ()=>{ location.reload();});
+btnCancelarSubida.addEventListener('click', ()=>{ location.reload();});
+bntCopyLink.addEventListener('click', copylink);
+btnDownloadGif.addEventListener('click', downlaodGif);
+btnReady.addEventListener('click', toMygifos);
 
-btnCreateGifo[0].hidden=true;
-btnDropdown[0].hidden=true;
-btnMisGifos[0].hidden=true;
+btnCreateGifo[0].hidden=true; 
+btnDropdown[0].hidden=true; 
+btnMisGifos[0].hidden=true; 
 sectionMisGifos[0].hidden=false;
 
 //Acciones para la página 1
-pag1[0].hidden=false;
-pag2[0].hidden=true;
-pag3[0].hidden=true;
-pag4[0].hidden=true;
-pag5[0].hidden=true;
-pag6[0].hidden=true;
+pag1[0].hidden=false; 
+pag2[0].hidden=true; 
+pag3[0].hidden=true; 
+pag4[0].hidden=true; 
+pag5[0].hidden=true; 
+pag6[0].hidden=true; 
 timerBox[0].hidden=true;
 
 //btnComenzar
@@ -58,7 +68,7 @@ function pagina2(){
     pag1[0].hidden=true;
     pag2[0].hidden=false;
     sectionMisGifos[0].hidden=true;
-    // getStreamAndRecord(video);
+    getStreamAndRecord(video);
 
 }
 //btnCapturar
@@ -88,30 +98,30 @@ function pagina3(){
         cs++;
         timerBox[0].innerText = (ho<10?"0"+ho:ho)+":"+(mn<10?"0"+mn:mn)+":"+(sg<10?"0"+sg:sg)+":"+(cs<10?"0"+cs:cs); 
     },10);
-    // function startVideoRecord() { 
-    //     navigator.mediaDevices.getUserMedia({ // pide permisos al user para usar dispositivos
-    //       audio: false,
-    //       video: {
-    //       height: { max: 480 }
-    //       } 
-    //       })
-    //       .then(function(stream) { // si el user da permisos, acciona la cámara
-    //       video1.srcObject = stream; //permite mostrar dentro del tag video lo que captura la cámara
-    //       video1.play();
-    //       recorder = RecordRTC(stream, { //se crea el objeto recorder de la librería 
-    //         type: 'gif',
-    //         frameRate: 1,
-    //         quality: 10,
-    //         width: 360,
-    //         hidden: 240,
-    //         onGifRecordingStarted: function() { 
-    //          console.log('started')
-    //        },
-    //       }); 
-    //       recorder.startRecording(); //comienza la grabación automáticamente
-    //      })
-    // }
-    // startVideoRecord();
+    function startVideoRecord() { 
+        navigator.mediaDevices.getUserMedia({ // pide permisos al user para usar dispositivos
+          audio: false,
+          video: {
+          height: { max: 480 }
+          } 
+          })
+          .then(function(stream) { // si el user da permisos, acciona la cámara
+          video1.srcObject = stream; //permite mostrar dentro del tag video lo que captura la cámara
+          video1.play();
+          recorder = RecordRTC(stream, { //se crea el objeto recorder de la librería 
+            type: 'gif',
+            frameRate: 1,
+            quality: 10,
+            width: 360,
+            hidden: 240,
+            onGifRecordingStarted: function() { 
+             console.log('started')
+           },
+          }); 
+          recorder.startRecording(); //comienza la grabación automáticamente
+         })
+    }
+    startVideoRecord();
 }
 function repeat(){
     preViewGifo.src='';
@@ -132,48 +142,48 @@ function pagina4(){
     clearInterval(timer);
     pag3[0].hidden=true;
     pag4[0].hidden=false;
-    // stopRecord()
-    // video1.pause();
-    // function stopRecord() { 
-    //     recorder.stopRecording(function(){ //para la grabación 
-    //       gifDone = this.getBlob(); //genera el archivo gif blob
-    //       objectUrl = this.toURL(); //guarda la url del gif para dsp mostrar en tag img como src
-    //       form.append('file', gifDone, 'myGif.gif'); //arma el archivo q luego se envia al request POST
-    //     })
-    // }
-    // console.log(URL.createObjectURL(form.get('file')));
-    // preViewGifo.src=URL.createObjectURL(form.get('file'));
+    stopRecord()
+    video1.pause();
+    function stopRecord() { 
+        recorder.stopRecording(function(){ //para la grabación 
+          gifDone = this.getBlob(); //genera el archivo gif blob
+          objectUrl = this.toURL(); //guarda la url del gif para dsp mostrar en tag img como src
+          form.append('file', gifDone, 'myGif.gif'); //arma el archivo q luego se envia al request POST
+        })
+    }
+    console.log(URL.createObjectURL(form.get('file')));
+    preViewGifo.src=URL.createObjectURL(form.get('file'));
 }
-
 //btnSubirGuifo
 function pagina5(){
     pag4[0].hidden=true;
     timerBox[0].hidden=true;
     pag5[0].hidden=false;
-}
-function pagina6(){
     a=0;
     b=0;
-
+    
+    time=window.setInterval(function(){
+        if(a==99){
+            a=0;
+            b++;
+            if(b==3){
+                b=0;
+                clearInterval(time);
+                btnCancelarSubida.disabled=true;
+                txtTags.disable=true;
+                strTags=txtTags.value;
+                //Colocar metodo post a giphy
+                postGif();
+                pagina6();
+            }
+        }   
+        a++;
+    },10);
+}
+function pagina6(){
     pag5[0].hidden=true;
     timerBox[0].hidden=true;
     pag6[0].hidden=false;
-    window.setInterval(console.log("hola"),10);
-
-    // postGif();
-
-}
-function time(){
-    if(a==99){
-        a=0;
-        b++;
-        if(b==3){
-            b=0;
-            // btnCancelarSubida.enable=false;
-        }
-    }   
-    cs++;
-    document.getElementById('span').innerHTML+=(sg<10?"0"+sg:sg)+":"+(cs<10?"0"+cs:cs); 
 }
 function getStreamAndRecord (media) {  
     navigator.mediaDevices.getUserMedia({ 
@@ -189,10 +199,11 @@ function getStreamAndRecord (media) {
  }
 function postGif(){
     var formdata = new FormData();
+
     formdata.append("api_key", "M2w3WvZMLnWs5ra5f7CsLTKJEwaGWD1O");
     formdata.append("username", "jorgeanibalsardon");
-    formdata.append("source_image_url", "https://gifsanimados.de/img-gifsanimados.de/l/los-simpson/los-simpson-lluvia.gif");
-    formdata.append("tags", "lluvia, simpsons");
+    formdata.append("file", form.get('file')/*"https://gifsanimados.de/img-gifsanimados.de/l/los-simpson/los-simpson-lluvia.gif"*/);
+    formdata.append(strTags);
     var requestOptions = {
         method: 'POST',
         body: formdata,
@@ -200,6 +211,30 @@ function postGif(){
     };
     fetch("https://upload.giphy.com/v1/gifs", requestOptions)
     .then(response => response.text())
-    .then(result => console.log(result))
+    .then(result => addToMyGifosLsit(result.data.id))
     .catch(error => console.log('error', error));
+}
+function addToMyGifosLsit(id){
+    objeto.push(id);
+    console.table(objeto);
+    localStorage.setItem("myGifos", JSON.stringify(objeto));
+    console.table(JSON.parse(guardado));
+}
+function downlaodGif(){
+    txtfinalUrl.value=gifoUrl+"-552da5rraAST$69";
+}
+function toMygifos(){
+    window.location.href='index.html';
+}
+function copylink(){
+    txtfinalUrl.select();
+    try {
+        var status = document.execCommand('copy');
+        if(!status){
+            message.innerText="No se pudo copiar el link";
+        }else{
+            message.innerText="El link fue copiado con éxito";
+        }
+    } catch (err) {
+    }
 }
